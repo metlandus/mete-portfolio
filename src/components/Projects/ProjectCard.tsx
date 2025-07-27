d0c25a70270e3f59e76222166f52bf0528ec556e
Mete <metlandus@hotmail.com.tr>
2025-07-28 00:29:04 +0300

    Icons for projects are done

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,6 +1,19 @@
 import { Project } from '@/lib/types'
 import Image from 'next/image'
 import { Earning, GithubIcon, Likes, PreviewIcon, Star, Timer } from '../../utils/icons'
+import {
+  FaReact as React,
+  FaHtml5 as Html,
+  FaCss3Alt as Css,
+  FaNodeJs as Nodejs,
+} from 'react-icons/fa'
+import {
+  SiJavascript as JavaScript,
+  SiMongodb as Mongodb,
+  SiTypescript as Typescript,
+  SiExpress as Expressjs,
+} from 'react-icons/si'
+import { RiNextjsFill as Nextjs, RiTailwindCssFill as Tailwind } from 'react-icons/ri'
 
 const IconText: React.FC<{ icon: string; text: string }> = ({ icon, text }) => (
   <li className="flex gap-2">
@@ -16,6 +29,36 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ data }) => {
   const { title, shortDescription, livePreview, githubLink, type, cover, techStack } = data
 
+  const iconMap: { [key: string]: React.ComponentType<{ className?: string }> } = {
+    react: React,
+    javascript: JavaScript,
+    html: Html,
+    css: Css,
+    nodejs: Nodejs,
+    mongodb: Mongodb,
+    typescript: Typescript,
+    nextjs: Nextjs,
+    express: Expressjs,
+    tailwindcss: Tailwind,
+  }
+
+  const iconColorMap: { [key: string]: string } = {
+    react: 'text-blue-400',
+    javascript: 'text-yellow-400',
+    html: 'text-orange-500',
+    css: 'text-blue-500',
+    nodejs: 'text-green-500',
+    mongodb: 'text-green-400',
+    typescript: 'text-blue-600',
+    nextjs: 'text-white',
+    express: 'text-gray-400',
+    tailwindcss: 'text-cyan-400',
+  }
+
+  const iconSizeMap: { [key: string]: string } = {
+    nextjs: 'w-8 h-8',
+  }
+
   return (
     <div className="bg-secondary border-border flex flex-col justify-between rounded-[14px] border p-5">
       <div className="flex items-start justify-between gap-2">
@@ -30,15 +73,16 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ data }) => {
             )}
           </div>
           <ul className="mt-3 flex flex-col flex-wrap gap-2 sm:flex-row sm:gap-4">
-            {techStack && techStack.map((el, index) => <li key={index}>{el}</li>)}
-            {/* {(visitors || numberOfSales) && (
-              <IconText text={(visitors || numberOfSales)?.toString() || ''} icon={Likes} />
-            )}
-            {siteAge && <IconText text={siteAge} icon={Timer} />}
-            {earned && <IconText text={earned} icon={Earning} />}
-            {(ratings || githubStars) && (
-              <IconText text={(ratings || githubStars)?.toString() || ''} icon={Star} />
-            )} */}
+            {techStack.map((tech, index) => {
+              const IconComponent = iconMap[tech.toLowerCase()]
+              const colorClass = iconColorMap[tech.toLowerCase()] || 'text-accent'
+              const sizeClass = iconSizeMap[tech] || 'w-7 h-7'
+              return IconComponent ? (
+                <li>
+                  <IconComponent key={index} className={`${colorClass} ${sizeClass} h-7 w-7`} />
+                </li>
+              ) : null
+            })}
           </ul>
         </div>
         <figure className="flex justify-end overflow-hidden">
